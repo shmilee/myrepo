f656ec4043be817f719dd09fe8ba4aff7cc58c4b
shmilee <shmilee.zju@gmail.com>
2016-02-29 12:44:16 +0800

    aurweb RPC Interface v=5

--- lib/dl_aur_info.sh
+++ lib/dl_aur_info.sh
@@ -5,7 +5,7 @@ export TEXTDOMAIN='myrepo'
 export TEXTDOMAINDIR='/usr/share/locale'
 
 source /etc/myrepo.conf
-INFOURL="$AURURL/rpc.php?type=info"
+INFOURL="$AURURL/rpc/?v=5\&type=info"
 if [ x$1 = 'x-s' ]; then
     source $(dirname $0)/base.sh
     msg2() {
@@ -37,13 +37,13 @@ if curl -LfGs --data-urlencode arg="$pkg" $INFOURL >${file_p}.tmp; then
         echo "0" > ${file_p}
     else
         msg2 "$(gettext "1. about Version ...")"
-        sed 's/.*Version\":\"//;s/\",\"Description.*//' ${file_p}.tmp >${file_p}
+        sed 's/.*Version\":\"//;s/\",\".*//' ${file_p}.tmp >${file_p}
         msg2 "$(gettext "2. about tarball URL path ...")"
         echo >>${file_p}
-        sed 's/^.*URLPath\":\"//;s/\",\"CategoryID.*//;s/\\//g' ${file_p}.tmp >>${file_p}
+        sed 's/^.*URLPath\":\"//;s/\",\".*//;s/\\//g' ${file_p}.tmp >>${file_p}
         msg2 "$(gettext "3. about Maintainer ...")"
         echo >>${file_p}
-        sed 's/.*Maintainer\":\"//;s/\",\"FirstSubmitted.*//' ${file_p}.tmp >>${file_p}
+        sed 's/.*Maintainer\":\"//;s/\",\".*//' ${file_p}.tmp >>${file_p}
     fi
     exit 0
 else
